18c6e9d8343ed14eaf604eab8203cb60d00bd9d2
lucas <lijie.lucas@hotmail.com>
2024-12-01 21:39:17 +1300

    改为用类写

--- Lucas-project/main.cpp
+++ Lucas-project/main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 class Solution {
-public:
+    public:
     int* arr1, *arr2;
-    int arrSize1,arrSize2;
+    int arrSize1, arrSize2, target;
     void inputNum() {
         cout << "请输入数组arr1的长度：";
         cin >> arrSize1;
@@ -18,6 +18,7 @@ public:
             cout << " " << arr1[i] << " ";
         }
         cout << "}" << endl;
+        cout << "========" << endl;
 
         cout << "请输入数组arr2的长度：";
         cin >> arrSize2;
@@ -25,27 +26,32 @@ public:
         for(int j = 0; j < arrSize2; j++) {
             cout << "第" << j+1 << "个元素,请输入：";
             cin >> arr2[j];
+            // cin.ignore();
         }
         cout << "arr1[" << arrSize2 << "]={";
         for(int j = 0; j < arrSize2; j++) {
             cout << " " << arr2[j] << " ";
         }
         cout << "}" << endl;
+        cout << "========" << endl;
+
+        cout << "请输入目标值：";
+        cin >> target;
 
         cout << endl << "=计算结果=" << endl;
     }
-    void isNum(int* arr1,int* arr2, int arrSize1,int arrSize2) {
+    void isNum(const int* arr1,const int* arr2, const int arrSize1, const int arrSize2) const {
         bool found = false;
         for (int i = 0; i < arrSize1; i++) {
             for (int j = 0; j < arrSize2; j++) {
-                if (arr1[i] + arr2[j] == 10) {
-                    cout << "arr1["<<i<<"]+arr2["<<j<<"]=10" << endl;
+                if (arr1[i] + arr2[j] == target) {
+                    cout << "arr1["<<i<<"]+arr2["<<j<<"]=" << target << endl;
                     found = true;
                 }
             }
         }
         if (!found) {
-            cout << "数组中元素相加都不等于10"<<endl;
+            cout << "数组中元素相加都不等于" << target << endl;
         }
     }
 };
